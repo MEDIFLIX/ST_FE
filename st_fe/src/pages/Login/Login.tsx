c517c675e5a4976f8e5e9051f9c6f58c53096ab0
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-15 01:22:26 +0900

    feat : Login 컴포넌트 추가

--- st_fe/src/pages/Login/Login.tsx
+++ st_fe/src/pages/Login/Login.tsx
@@ -1,7 +1,42 @@
+import { Button, TextField } from '@mui/material';
+import { useNavigate } from 'react-router';
+import styled from 'styled-components';
 import { CenterContainer } from '../../styles/GlobalStyle';
 
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+`;
+
 const Login = () => {
-  return <CenterContainer>Login</CenterContainer>;
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate(`/main`);
+  };
+
+  return (
+    <CenterContainer>
+      <LoginContainer>
+        <TextField
+          id="standard-basic"
+          label="ID"
+          variant="standard"
+          sx={{ marginBottom: '30px' }}
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          variant="standard"
+          sx={{ marginBottom: '50px' }}
+        />
+        <Button variant="contained" sx={{ backgroundColor: 'black' }} onClick={handleLogin}>
+          Login
+        </Button>
+      </LoginContainer>
+    </CenterContainer>
+  );
 };
 
 export default Login;
