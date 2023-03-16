743bf867e9c360f933230b9906d90a6bd4e3338e
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-16 14:09:44 +0900

    feat : grid 배치 완료

--- st_fe/src/pages/MainPage/MainPage.tsx
+++ st_fe/src/pages/MainPage/MainPage.tsx
@@ -4,19 +4,90 @@ import SideBar from '../../components/SideBar';
 
 const MainContainer = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 954px;
   background-color: white;
   position: relative;
 `;
 
 const ContentContainer = styled.div`
   position: absolute;
+  display: flex;
   top: 70px;
   left: 10vw;
   width: 90vw;
-  height: calc(100vh - 70px);
-
   background-color: #e7e7e7;
+  height: 100%;
+`;
+
+const DashContainer = styled.section`
+  flex: 2.9;
+  height: 100%;
+`;
+
+const MemoContainer = styled.section`
+  flex: 1;
+  height: 100%;
+  background-color: #fbfbfb;
+`;
+
+const Title = styled.div`
+  color: #e31e26;
+  font-size: 25px;
+  margin-top: 20px;
+  margin-left: 20px;
+  height: 50px;
+`;
+
+// type ContentBoxProps = {
+//   width: string;
+//   height: string;
+// };
+
+const ContentBox = styled.div`
+  background-color: #ffffff;
+  border: 7px solid #d6cece;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ContentGridBox = styled.div`
+  display: grid;
+  position: relative;
+  height: calc(100% - 96px);
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-bottom: 26px;
+  grid-gap: 16px;
+
+  grid-template-areas:
+    'chart chart analysis analysis analysis'
+    'graph graph graph item1 item2'
+    'weekContent weekContent weekContent diagram diagram';
+
+  grid-template-rows: 330px 233px 226px;
+
+  .grid-chart {
+    grid-area: chart;
+  }
+  .grid-analysis {
+    grid-area: analysis;
+  }
+  .grid-graph {
+    grid-area: graph;
+  }
+  .grid-item1 {
+    grid-area: item1;
+  }
+  .grid-item2 {
+    grid-area: item2;
+  }
+  .grid-content {
+    grid-area: weekContent;
+  }
+  .grid-diagram {
+    grid-area: diagram;
+  }
 `;
 
 const MainPage = () => {
@@ -25,7 +96,19 @@ const MainPage = () => {
       <NavBar />
       <SideBar />
       <ContentContainer>
-        <div>hi</div>
+        <DashContainer>
+          <Title>Mediflix DashBoard</Title>
+          <ContentGridBox>
+            <ContentBox className={'grid-chart'}>chart</ContentBox>
+            <ContentBox className={'grid-analysis'}>analysis</ContentBox>
+            <ContentBox className={'grid-graph'}>graph</ContentBox>
+            <ContentBox className={'grid-item1'}>item1</ContentBox>
+            <ContentBox className={'grid-item2'}>item2</ContentBox>
+            <ContentBox className={'grid-content'}>content</ContentBox>
+            <ContentBox className={'grid-diagram'}>diagram</ContentBox>
+          </ContentGridBox>
+        </DashContainer>
+        <MemoContainer></MemoContainer>
       </ContentContainer>
     </MainContainer>
   );
