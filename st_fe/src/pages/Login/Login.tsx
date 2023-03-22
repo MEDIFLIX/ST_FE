06f8fa1e702f5fc8c236646ad0ab4a8635665424
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-22 11:43:36 +0900

    feat : 로그인 완료

--- st_fe/src/pages/Login/Login.tsx
+++ st_fe/src/pages/Login/Login.tsx
@@ -1,7 +1,9 @@
 import { Button, TextField } from '@mui/material';
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 import { CenterContainer } from '../../styles/GlobalStyle';
+import * as API from '../../api/API';
 
 const LoginContainer = styled.div`
   display: flex;
@@ -10,12 +12,29 @@ const LoginContainer = styled.div`
 `;
 
 const Login = () => {
+  const [idValue, setIdValue] = useState<string>('');
+  const [pwValue, setPwValue] = useState<string>('');
+
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate(`/main`);
+  const handleLogin = async () => {
+    const loginInfo = {
+      id: idValue,
+      pwd: pwValue,
+    };
+    const res = await API.post('/admin/login', loginInfo);
+    if (res) {
+      navigate(`/main`);
+    }
   };
 
+  const handleIdChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setIdValue(e.currentTarget.value);
+  };
+
+  const handlePwChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPwValue(e.currentTarget.value);
+  };
   return (
     <CenterContainer>
       <LoginContainer>
@@ -24,12 +43,16 @@ const Login = () => {
           label="ID"
           variant="standard"
           sx={{ marginBottom: '30px' }}
+          value={idValue}
+          onChange={handleIdChange}
         />
         <TextField
           id="standard-basic"
           label="Password"
           variant="standard"
           sx={{ marginBottom: '50px' }}
+          value={pwValue}
+          onChange={handlePwChange}
         />
         <Button variant="contained" sx={{ backgroundColor: 'black' }} onClick={handleLogin}>
           Login
